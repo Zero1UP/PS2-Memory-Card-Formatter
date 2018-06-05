589d39a672c174b46a5197af5130b5b140c438f1
Based-Skid <tvbxspprt@gmail.com>
2018-06-05 19:24:05 -0400

    Update main.c
    Removed the Module Loading Code from the gotoOSDSYS() function, Its not needed.

--- main.c
+++ main.c
@@ -476,10 +476,6 @@ void gotoOSDSYS()
 	ResetIOP();
 	scr_printf("Exiting To OSDSYS..\n");
 	sleep(3);
-	SifLoadFileInit(); 
-	SifLoadModule("rom0:SIO2MAN", 0, NULL);
-	SifLoadModule("rom0:MCMAN", 0, NULL);
-	SifLoadModule("rom0:MCSERV", 0, NULL);
 	LoadExecPS2("rom0:OSDSYS", 0, NULL);
 	
 }
