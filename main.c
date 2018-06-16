1c1b9d4c5b2e10d5fe745e1cfe4544ff302ea63e
Based-Skid <tvbxspprt@gmail.com>
2018-06-16 03:18:54 -0400

    Update main.c

--- main.c
+++ main.c
@@ -58,7 +58,7 @@ static int mc_Type, mc_Free, mc_Format;
 
 //vars
 	char *appName = "Mass Format Utility ";
-	char *appVer = "Version 0.3 ";
+	char *appVer = "Version 0.4 ";
 	char *appAuthor = "Created By: 1UP & Based_Skid. Copyright \xa9 2018\n";
 	char *help = "Special thanks to SP193 for all the help! \n";
 	char *appNotice = "Notice: This May Not be Compatible With all PS2 Models!\n";
